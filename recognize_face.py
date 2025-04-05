0d948a16efd5fecaffb957560452bee5c3ea1bb4
Pranav Sharma <pranavsharma12355@gmail.com>
2025-04-05 14:59:51 +0530

    take photo while recognizing person from image

--- recognize_face.py
+++ recognize_face.py
@@ -5,29 +5,51 @@ import pickle
 
 def recognize_face():
 
-    img_path="Testing Images/PS1.jpg"
+    # img_path="Testing Images/PS1.jpg"
     height=200
     width=200
     X=[]
 
+    cap = cv2.VideoCapture(0)
+
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-    image=cv2.imread(img_path)
-    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    print("Press 's' to start capturing images...")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Couldn't access the camera.")
+            break
+
+        cv2.imshow("Press 's' to start", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('s'): 
+            break
+
+    cv2.destroyAllWindows()
+
+    ret, frame = cap.read()
+    if not ret:
+        return
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     for (x, y, w, h) in faces:
-            face_roi = gray[y:y+h, x:x+w] 
-            face_resized = cv2.resize(face_roi, (200, 200))
-            flatten_img=face_resized.flatten()
-            X.append(flatten_img)
+        face_roi = gray[y:y+h, x:x+w] 
+        face_resized = cv2.resize(face_roi, (200, 200))
+        flatten_img=face_resized.flatten()
+        X.append(flatten_img)
     
     X=np.array(X)
     
     # plt.imshow(X.reshape(height,width), cmap="gray")
     # plt.show()   
 
-    filename="./models/face_recognition_model.pkl"
+    filename="./models/model_kneighbors.pkl"
     model = pickle.load(open(filename,"rb"))
 
     y_predict = model.predict(X)
